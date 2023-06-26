ff73194554f75656da0efa2b684868ba95bd2248
Bobby Kiaie <bobbykiaie@gmail.com>
2023-06-26 06:46:24 -0700

    Initial Commit

---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Babak Kiaie on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
